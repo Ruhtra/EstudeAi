aef10e771b776b9adfd7061354ce26d42feebaec
Ruhtra <kawanarthurskate@gmail.com>
2025-01-30 01:03:52 -0300

    Fixed a problema with thema

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,17 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+import type * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider
+      {...props}
+      defaultTheme={window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark": "light"}
+      themes={["light", "dark"]}
+      enableSystem={false}
+      storageKey="theme"
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
